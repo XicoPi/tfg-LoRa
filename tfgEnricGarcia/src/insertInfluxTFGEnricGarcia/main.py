9f93908a147705f9be8d9d7df478de0ca5e36dff
XicoPi <5172714+XicoPi@users.noreply.github.com>
2021-07-20 08:33:12 +0200

    finish main

--- tfgEnricGarcia/src/insertInfluxTFGEnricGarcia/main.py
+++ tfgEnricGarcia/src/insertInfluxTFGEnricGarcia/main.py
@@ -77,27 +77,40 @@ def influx_point(device_id: str, timestamp: int, measurement: str, value: float)
 
     return result
 
+def get_lora_params(msg_dict: dict) -> dict:
 
+    lora_data = msg_dict["uplink_message"]["settings"]["data_rate"]["lora"]
+    msg_dict["uplink_message"]["settings"].pop("data_rate")
+    lora_data.update(msg_dict["uplink_message"]["settings"])
+    lora_data.pop("timestamp"); lora_data.pop("time"); lora_data.pop("coding_rate")
+
+    rx_metadata = msg_dict["uplink_message"]["rx_metadata"][0]
+    rx_metadata.pop("gateway_ids"); rx_metadata.pop("location"); rx_metadata.pop("uplink_token"); rx_metadata.pop("time"); rx_metadata.pop("timestamp")
+    lora_data.update(rx_metadata)
+    return lora_data
 
 def get_msg_info(msg_dict: Parsed_Msg_t) -> Tuple:
     """
     Returned value (tuple):
     (device_id: int, sens_data: dict, timestamp: time)
     """
+    
     device_id = msg_dict["end_device_info"]["device_id"]
     sens_data = msg_dict["uplink_message"]["decoded_payload"]
     timestamp = msg_dict["received_at"]
-    #print(sens_data, device_id)
+    print(sens_data, device_id)
     for deviceObj in ttnApplication:
         print(deviceObj.getId())
         if (deviceObj.getId() == device_id):
             sens_data = deviceObj.executeCallback(sens_data)
             break;
-    sens_data.update(msg_dict["uplink_message"]["settings"]["data_rate"]["lora"])
-    msg_dict["uplink_message"]["settings"].pop("data_rate")
-    sens_data.update(msg_dict["uplink_message"]["settings"])
-    sens_data.pop("timestamp"); sens_data.pop("time")
-    return (device_id, sens_data, timestamp)
+
+    #LoRa Params register
+    result = {}
+    result["data_sensors"] = sens_data
+    result["lora_com"] = get_lora_params(msg_dict)
+    
+    return (device_id, result, timestamp)
 
 def influx_points(msg_dict: Parsed_Msg_t) -> list:
     """
@@ -108,11 +121,15 @@ def influx_points(msg_dict: Parsed_Msg_t) -> list:
         if "decoded_payload" not in msg_dict["uplink_message"].keys():
             raise(ValueError)
 
-        device_id, sens_data, timestamp = get_msg_info(msg_dict)
-        print(sens_data)
+        device_id, info, timestamp = get_msg_info(msg_dict)
+        print(info)
         result = []
-        for sensor, value in sens_data.items():
+        #Uplink data sensors
+        for sensor, value in info["data_sensors"].items():
             result.append(influx_point(device_id, int(timestamp), sensor, float(value)))
+
+        for lora_communication_param, value in info["lora_com"].items():
+            result.append(influx_point(device_id, int(timestamp), lora_communication_param, float(value)))
     except ValueError:
         print(colored("EXCEPTION - Not information in the message", "red"))
         result = []
@@ -123,6 +140,7 @@ def influx_insert(iDBconf: dict, msg_dict: dict):
     """
     Insert the data from device's sensors into DDBB
     """
+    result = 0
     try:
         client = InfluxDBClient(iDBconf["HOST"],
                                 iDBconf["PORT"],
@@ -132,9 +150,10 @@ def influx_insert(iDBconf: dict, msg_dict: dict):
 
         client.write_points(influx_points(msg_dict))
     except requests.exceptions.ConnectionError:
-        pass
+        result = -1
     finally:
         client.close()
+    return result
 
 
 def main():
@@ -157,12 +176,15 @@ def main():
             except socket.timeout:
                 continue
             except requests.exceptions.ConnectionError:
-                continue
+                result = 1
+                break
 
             msg_dict = msg_parse(MQTT_message.payload)
             #print(msg_dict)
             print(colored("Message received from device: \n\t" + msg_dict["end_device_info"]["device_id"], "green"))
-            influx_insert(iDBconf, msg_dict)
+            if (influx_insert(iDBconf, msg_dict) != 0):
+                result = 1
+                break
     except KeyboardInterrupt:
         result = 0
 
@@ -172,5 +194,7 @@ def main():
 if __name__ == "__main__":
 
     status = main()
+    while (status == 1):
+        status = main()
     sys.exit(status)
     
